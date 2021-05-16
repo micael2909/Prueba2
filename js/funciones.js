618d795f46cf94107fc3304224365c91a1803789
micael2909 <micael.moscoso@hotmail.com>
2021-05-16 15:43:23 -0400

    Micael-Commit-20210516-1543

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,14 @@
 fn_limpiarEtiqueta();
 
+var loading = $('#sp_cargando').hide();
+$(document)
+  .ajaxStart(function () {
+    loading.show();
+  })
+  .ajaxStop(function () {
+    loading.hide();
+});
+
 function fn_limpiarEtiqueta(){
     $("#alerta").removeClass("alert-success");
     $("#alerta").removeClass("alert-danger");
@@ -22,25 +31,89 @@ $("#btn_registro").click(function(){
     if(rut == ""){
         countErrores++;
         txtErrores += "Debe ingresar un Rut <br>";
-    }
-    else {
-        $.getJSON('https://api.libreapi.cl/rut/validate/',{rut:rut})
-            .done(function(json){
-                if(!json.data.valid){
-                    $("#alerta").prepend("Rut invalido <br>");
-                }
-            })
-    }
-
-    if(nombre == ""){
-        countErrores++;
-        txtErrores += "El Campo Nombre no puede estar vacío <br>";
-    }
-
-    if(countErrores > 0){
+        if(nombre == ""){
+            countErrores++;
+            txtErrores += "El campo Nombre no puede estar vacío <br>";
+        }
+        if(apellidop == ""){
+            countErrores++;
+            txtErrores += "El campo Apellido Paterno no puede estar vacío <br>";
+        }
+        if(apellidom == ""){
+            countErrores++;
+            txtErrores += "El campo Apellido Materno no puede estar vacío <br>";
+        }
+        if(correo == ""){
+            countErrores++;
+            txtErrores += "El campo Correo Electrónico no puede estar vacío <br>";
+        }else{
+            var formatoCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+            if(!formatoCorreo.test(correo)){
+                countErrores++;
+                txtErrores += "Correo inválido"
+            }
+        }
+        if(tipocuenta == "-- SELECCIONE --"){
+            countErrores++;
+            txtErrores += "Debe seleccionar un tipo de cuenta <br>";
+        }
         $("#alerta").addClass("alert-danger");
         $("#alerta").css("visibility","visible");
         $("#alerta").append(txtErrores);
     }
+    else{
+        $.when($.getJSON('https://api.libreapi.cl/rut/validate/',{rut:rut})
+        .done(function(json){
+        if(!json.data.valid){
+            $("#alerta").prepend("Rut inválido <br>");
+            countErrores++;
+        }
+        }).fail(function(json){
+                $("#alerta").addClass("alert-danger");
+                $("#alerta").css("visibility","visible");
+                $("#alerta").append("Formato de rut inválido");
+        })
+        ).done(function(){
+
+            if(nombre == ""){
+                countErrores++;
+                txtErrores += "El campo Nombre no puede estar vacío <br>";
+            }
+            if(apellidop == ""){
+                countErrores++;
+                txtErrores += "El campo Apellido Paterno no puede estar vacío <br>";
+            }
+            if(apellidom == ""){
+                countErrores++;
+                txtErrores += "El campo Apellido Materno no puede estar vacío <br>";
+            }
+            if(correo == ""){
+                countErrores++;
+                txtErrores += "El campo Correo Electrónico no puede estar vacío <br>";
+            }else{
+                var formatoCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+                if(!formatoCorreo.test(correo)){
+                    countErrores++;
+                    txtErrores += "Correo inválido";
+                }
+            }
+
+            if(tipocuenta == "-- SELECCIONE --"){
+                countErrores++;
+                txtErrores += "Debe seleccionar un tipo de cuenta <br>";
+            }
+
+            if(countErrores > 0){
+                $("#alerta").addClass("alert-danger");
+                $("#alerta").css("visibility","visible");
+                $("#alerta").append(txtErrores);
+            }else{
+                $('#m_registrado').modal('show');
+            }
+        })
+    }
 })
 
+$("#btn_limpiar").click(function(){
+    $("#formregistro").trigger("reset");
+})
