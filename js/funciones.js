6dc66133b38f24b417f071189c7e48ee7a123f45
micael2909 <micael.moscoso@hotmail.com>
2021-05-16 02:35:43 -0400

    Micael-Commit-20210516-0235

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,46 @@
-function fn_validar_rut(rut){
-    $.getJSON('https://api.libreapi.cl/rut/validate/',{rut:rut}).done(function(json){
-        return json.data.valid;
-    }).fail(function(json){
-        alert("Error al Validar RUT")
-    })
+fn_limpiarEtiqueta();
+
+function fn_limpiarEtiqueta(){
+    $("#alerta").removeClass("alert-success");
+    $("#alerta").removeClass("alert-danger");
+    $("#alerta").css("visibility","hidden");
+    $("#alerta").html("");
 }
 
 
 $("#btn_registro").click(function(){
-    console.log(fn_validar_rut("19586995-2"));
+    fn_limpiarEtiqueta();
+    var rut = $("#txt_rut").val();
+    var nombre = $("#txt_nombre").val();
+    var apellidop = $("#txt_appaterno").val();
+    var apellidom = $("#txt_apmaterno").val();
+    var correo = $("#txt_email").val();
+    var tipocuenta = $("#cmb_tcuenta option:selected").text();
+    var countErrores = 0;
+    var txtErrores = "";
+
+    if(rut == ""){
+        countErrores++;
+        txtErrores += "Debe ingresar un Rut <br>";
+    }
+    else {
+        $.getJSON('https://api.libreapi.cl/rut/validate/',{rut:rut})
+            .done(function(json){
+                if(!json.data.valid){
+                    $("#alerta").prepend("Rut invalido <br>");
+                }
+            })
+    }
+
+    if(nombre == ""){
+        countErrores++;
+        txtErrores += "El Campo Nombre no puede estar vacío <br>";
+    }
+
+    if(countErrores > 0){
+        $("#alerta").addClass("alert-danger");
+        $("#alerta").css("visibility","visible");
+        $("#alerta").append(txtErrores);
+    }
 })
+
